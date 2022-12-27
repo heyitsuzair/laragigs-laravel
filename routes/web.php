c14162e6389b4cf5b3d929a007653dd6c7254e8c
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-12-28 02:44:53 +0500

    Improvements

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,10 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/listing/{id}', function ($id) {
+Route::get('/listing/{listing}', function (Listing $listing) {
+
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });
 
