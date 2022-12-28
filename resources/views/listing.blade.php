f4d0e2a66b957e9bb834e97f20f62a750878c4e9
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-12-28 05:42:50 +0500

    Components

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,11 +1,11 @@
 @extends('layout')
 @section('content')
 @include('partials._search')
-<a href="index.html" class="inline-block text-black ml-4 mb-4"
+<a href="/" class="inline-block text-black ml-4 mb-4"
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-<div class="bg-gray-50 border border-gray-200 p-10 rounded">
+<x-card class="bg-gray-50 border border-gray-200 rounded p-6">
     <div
         class="flex flex-col items-center justify-center text-center"
     >
@@ -67,6 +67,6 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
 </div>
 @endsection
