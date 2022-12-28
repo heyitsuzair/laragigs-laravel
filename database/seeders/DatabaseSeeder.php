a38b38f14ddfad2981ca015752879c6bf3d6a03a
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-12-29 04:39:46 +0500

    User And Listings Relation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
 
-        Listing::factory(5)->create();
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+        ]);
+
+        Listing::factory(5)->create(
+            [
+                'user_id' => $user->id,
+            ]
+        );
 
         // Listing::create(
         //     [
